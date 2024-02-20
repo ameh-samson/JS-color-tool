7b5d966e6c3232699915ef05b207fd64f004a018
Samson Ameh <amehsam69@gmail.com>
2024-02-20 13:48:10 +0100

    add convertHexToRGB function

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ const isValidHex = (hex) => {
   const trimmedHex = hex.replace("#", "");
   return trimmedHex.length === 3 || trimmedHex.length === 6;
 };
+
+const convertHexToRGB = (hex) => {
+  if (!isValidHex(hex)) return null;
+
+  let trimmedHex = hex.replace("#", "");
+
+  if (trimmedHex.length === 3) {
+    trimmedHex =
+      trimmedHex[0] +
+      trimmedHex[0] +
+      trimmedHex[1] +
+      trimmedHex[1] +
+      trimmedHex[2] +
+      trimmedHex[2];
+  }
+
+  const r = parseInt(trimmedHex.substring(0, 2), 16);
+  const g = parseInt(trimmedHex.substring(2, 4), 16);
+  const b = parseInt(trimmedHex.substring(4, 6), 16);
+
+  return { r, g, b };
+};
+
+console.log(convertHexToRGB("123"));
